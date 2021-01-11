84eb35796cc08f8b18aa4e84da40bce1591fc16c
Alex <Aleks031994@mail.ru>
2021-01-11 14:31:55 +0300

    second step modal window

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const modal = $.modal()
+const modal = $.modal({
+    title: 'Alex Modal',
+    closable: true,
+    content: `
+        <h4>Modal Window</h4>
+        <p>Lorem ipsum dolor sit.</p>
+    `,
+    width: '400px',
+    footerButtons: [
+        {text: 'Ok', type: 'primary', handler() {
+            console.log('Primary btn clicked')
+            modal.close()   
+        }},
+        {text: 'Cancel', type: 'danger', handler() {
+            console.log('Danger btn clicked')
+            modal.close() 
+        }}
+    ]
+})
 
